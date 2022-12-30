b3ed6193604c9704abd96583a8d6b9d5b6e00f88
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2022-12-30 16:11:53 +0900

    JS:Scroll opacity

--- js/main.js
+++ js/main.js
@@ -23,8 +23,13 @@
         messagaD: document.querySelector("#scroll__section_0 .main__message.d"),
       },
       values: {
+        //스크롤 비율로 애니메이션 적용(때문에 소수점)
+        //전체 1에서의 비율 0.1 = 10%
         //배열로 투명도[시작값,끝값] 값에 해당
-        messageA_opacity: [0, 1],
+        //세번째 원소를 객체로 추가
+        messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+        // messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
+        messageA_opacity_out: [1, 0, { start: 0.24, end: 0.3 }],
       },
     },
     //1
@@ -54,8 +59,11 @@
     //각 스크롤 섹션의 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+
       sceneInfo[i].objs.section.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+
+    yOffset = window.pageYOffset;
     //현재 스크롤 위치에 맞춰서 현재(활성화) 씬 세팅
     let totalScrollHeight = 0;
     //totalScrollHeight와 현재 위치 비교
@@ -71,8 +79,33 @@
   }
   function calcValues(values, currentYOffset) {
     let returnValue;
-    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
-    returnValue = scrollRatio * (values[1] - values[0] + values[0]);
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+
+    if (values.length === 3) {
+      //start ~ end사이에 애니메이션 실행
+      //valuse의 length가 3이거나 , valuse의 인덱스 2번이 값이 있는 경우
+      const partScrollStart = values[2].start * scrollHeight;
+      const partScrollEnd = values[2].end * scrollHeight;
+      const partScrollheight = partScrollEnd - partScrollStart;
+
+      if (
+        currentYOffset >= partScrollStart &&
+        currentYOffset <= partScrollEnd
+      ) {
+        returnValue =
+          ((currentYOffset - partScrollStart) / partScrollheight) *
+            (values[1] - values[0]) +
+          values[0];
+      } else if (currentYOffset < partScrollStart) {
+        returnValue = values[0];
+      } else if (currentYOffset > partScrollEnd) {
+        returnValue = values[1];
+      }
+    } else {
+      returnValue = scrollRatio * (values[1] - values[0] + values[0]);
+    }
+
     return returnValue;
     //기본적으로 리턴해주는 값이 있어야 계산된 결과값을 가져다 사용 가능
   }
@@ -82,17 +115,24 @@
     const objs = sceneInfo[currentScene].objs;
     const values = sceneInfo[currentScene].values;
     const currentYOffset = yOffset - prevScrollHeight;
-
-    console.log(currentScene);
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
     switch (currentScene) {
       case 0:
-        let messageA_opacity_in = calcValues(
-          values.messageA_opacity,
+        const messageA_opacity_in = calcValues(
+          values.messageA_opacity_in,
           currentYOffset
         );
-        objs.messagaA.style.opacity = messageA_opacity_in;
-        console.log(messageA_opacity_in);
+        const messageA_opacity_out = calcValues(
+          values.messageA_opacity_out,
+          currentYOffset
+        );
 
+        if (scrollRatio <= 0.22) {
+          objs.messagaA.style.opacity = messageA_opacity_in;
+        } else {
+          objs.messagaA.style.opacity = messageA_opacity_out;
+        }
         break;
       case 1:
         break;
@@ -106,6 +146,8 @@
   // 현재 활성화된 창
   function scrollLoop() {
     enterNewScene = false;
+    //변수가 true가되면 새로운 씬에 들어간 순간이다 판별
+    //그다음 스크롤 통해 바로 false처리
     prevScrollHeight = 0;
 
     for (let i = 0; i < currentScene; i++) {
